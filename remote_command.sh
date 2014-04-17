7332d68114271dbd7db445774c396a03907ea97d
Hendrik Morée <hendrik@seecr.nl>
2014-04-17 08:55:16 +0200

    HM: Default port 22 for ssh

--- remote_command.sh
+++ remote_command.sh
@@ -28,8 +28,12 @@ if [ "$mountPoint" == "/" ] || [ "$mountPoint" == "." ] || [ "$mountPoint" == "$
     )
     exit
 fi
+sshfsCommand=$(ps aux | grep sshfs | grep $mountPoint | awk -F'sshfs' '{print $2}' | head -n 1)
 SERVER_DIR=$(mount | grep $mountPoint | awk '{print $1}' | awk -F: '{print $2}' | head -n 1)
-SERVER_LOGIN=$(ps aux | grep sshfs | grep $mountPoint | awk '{print $(NF-1)}' | awk -F: '{print $1}' | head -n 1)
-SERVER_PORT=$(ps aux | grep sshfs | grep $mountPoint | awk '{print $(NF-2)}' | head -n 1)
+SERVER_LOGIN=$(echo $sshfsCommand | awk '{print $(NF-1)}' | awk -F: '{print $1}')
+SERVER_PORT=22
+if $(echo $sshfsCommand | grep "\-p" > /dev/null); then
+    SERVER_PORT=$(echo $sshfsCommand | awk -F'-p' '{print $2}' | awk '{print $1}')
+fi
 # echo REMOTE_USERNAME=${USER} ssh $SERVER_LOGIN -o SendEnv=REMOTE_USERNAME -p $SERVER_PORT "(cd $SERVER_DIR/$serverProjectDir; $COMMAND)"
 REMOTE_USERNAME=${USER} ssh $SERVER_LOGIN -o SendEnv=REMOTE_USERNAME -p $SERVER_PORT "(cd $SERVER_DIR/$serverProjectDir; $COMMAND)"

dcb454fa196886153b0365b882a11b5c78ebffa5
Hendrik Morée <hendrik@seecr.nl>
2015-05-20 16:02:37 +0200

    HM: Reload previous view

--- gitcommands.py
+++ gitcommands.py
@@ -100,6 +100,9 @@ class RemoteGitCommitClose(EventListener):
         #     command.addOption(o)
         command.addOption("--file .git/COMMIT_EDITMSG --cleanup strip")
         remoteCommand(view, command)
+        views = view.active_view().window.views()
+        preView = views[views.index(view) - 1]
+        preView.run_command("remote_git_st")
 
 class RemoteGitCommitAmend(RemoteGitCommit):
     options = ['--amend']
